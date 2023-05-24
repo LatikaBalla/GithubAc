62fb29e7e4b2d6bb9a2a2aaeda512bc6a6c3c2a4
LatikaBalla <114507652+LatikaBalla@users.noreply.github.com>
2023-05-24 20:22:36 +0530

    Generate Report

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,22 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require('cypress-mochawesome-reporter/plugin')(on);
     },
   },
+
+  
+  "reporter": "cypress-mochawesome-reporter",
+  "reporterOptions": {
+    "reportDir": "cypress/report",
+    "charts": true,
+    "saveHtml": true,
+    "json": true,
+    "reportPageTitle": "Legrande Cypress",
+    "reportFilename": "Report",
+    "embeddedScreenshots": true,
+    "inlineAssets": true,
+    "overwrite": true 
+  },
+   
 });
